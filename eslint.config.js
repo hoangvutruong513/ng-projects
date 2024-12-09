b9973e8c7440a64b2013322811fdf5529727f22e
vumac <hoangvutruong27894@gmail.com>
2024-12-09 19:14:33 +0800

    update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-const eslint = require("./node_modules/@eslint/js");
+const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
 const eslintConfigPrettier = require("eslint-config-prettier");
