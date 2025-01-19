794510f4f5b38aa87833520406e4eda2a0f2406e
vumac <hoangvutruong27894@gmail.com>
2025-01-19 13:25:31 +0800

    Add QoL packages and reconfig eslint + prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
 import angular from "angular-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  {
+    ignores: [".angular/", "dist/"],
+  },
   {
     languageOptions: {
       parserOptions: {
@@ -29,6 +34,27 @@ export default tseslint.config(
       "@angular-eslint/no-conflicting-lifecycle": "error",
       "@angular-eslint/no-duplicates-in-metadata-arrays": "error",
       "@angular-eslint/no-lifecycle-call": "error",
+    },
+  },
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
   {
