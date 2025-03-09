2720421874c80f90a4b9c6af73dce2697a2df9bb
vumac <hoangvutruong27894@gmail.com>
2025-03-09 17:23:06 +0800

    g

--- projects/test-ng-19/src/app/content-projection-experiment/content-projection-experiment.component.ts
+++ projects/test-ng-19/src/app/content-projection-experiment/content-projection-experiment.component.ts
@@ -1,10 +1,68 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import {
+  afterRenderEffect,
+  ChangeDetectionStrategy,
+  Component,
+  EmbeddedViewRef,
+  inject,
+  OnInit,
+  TemplateRef,
+  viewChild,
+  ViewContainerRef,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-test-component',
+  imports: [DatePipe],
+  template: ` <div>{{ viewChecked() | date: 'medium' }}</div> `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TestComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('Test Component is inited');
+  }
+  viewChecked() {
+    console.log('This is called when this test component is change detected');
+    return new Date();
+  }
+}
+
+@Component({
+  selector: 'app-content-in-template',
+  template: `
+    <ng-template #withContent>
+      <ng-content />
+    </ng-template>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ContentInTemplateComponent {
+  vcr = inject(ViewContainerRef);
+  template = viewChild('withContent', { read: TemplateRef<unknown> });
+  e = afterRenderEffect((onCleanup) => {
+    const template = this.template();
+    let viewRef: EmbeddedViewRef<unknown> | undefined;
+    if (template) {
+      viewRef = this.vcr.createEmbeddedView(template);
+    }
+    onCleanup(() => {
+      if (viewRef) {
+        const index = this.vcr.indexOf(viewRef);
+        this.vcr.remove(index);
+      }
+    });
+  });
+}
 
 @Component({
   selector: 'app-content-projection-experiment',
-  imports: [],
-  templateUrl: './content-projection-experiment.component.html',
-  styles: ``,
+  imports: [ContentInTemplateComponent, TestComponent],
+  template: `
+    <div>Hello World</div>
+    <app-content-in-template>
+      <app-test-component />
+    </app-content-in-template>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContentProjectionExperimentComponent {}
