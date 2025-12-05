1b979e0ff27cb0930a23d3fdfd3f5af32bb32976
vumac <hoangvutruong27894@gmail.com>
2025-12-05 23:55:51 +0800

    Create some pages

--- projects/route-reuse-strat/src/app/app.ts
+++ projects/route-reuse-strat/src/app/app.ts
@@ -1,13 +1,28 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+  template: `
+    <div>{{ count() }}</div>
+    <div>{{ doubleCount() }}</div>
+    <button (click)="increment()">Increment</button>
+    <router-outlet />
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
   protected readonly title = signal('route-reuse-strat');
+  readonly count = signal(0);
+  readonly doubleCount = computed(() => this.count() * 2);
+
+  increment() {
+    this.count.update((c) => c + 1);
+  }
 }
---
+++ projects/route-reuse-strat/src/app/feature/order/page/order-detail/order-detail.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-detail',
+  imports: [],
+  templateUrl: './order-detail.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class OrderDetail {}
