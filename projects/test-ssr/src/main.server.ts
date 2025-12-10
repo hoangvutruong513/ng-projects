0b0c62c6f39a738ae052114321b9548f0e6bde45
vumac <hoangvutruong27894@gmail.com>
2025-12-11 01:15:23 +0800

    Test server context

--- projects/test-ssr/src/main.server.ts
+++ projects/test-ssr/src/main.server.ts
@@ -1,3 +1,4 @@
+import { REQUEST } from '@angular/core';
 import {
   bootstrapApplication,
   BootstrapContext,
@@ -6,7 +7,11 @@ import {
 import { App } from './app/app';
 import { config } from './app/app.config.server';
 
-const bootstrap = (context: BootstrapContext) =>
-  bootstrapApplication(App, config, context);
+const bootstrap = (context: BootstrapContext) => {
+  const injector = context.platformRef.injector;
+  const request = injector.get(REQUEST);
+  console.log({ context, request });
+  return bootstrapApplication(App, config, context);
+};
 
 export default bootstrap;
