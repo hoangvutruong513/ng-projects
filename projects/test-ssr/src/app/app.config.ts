3c9166a45fc0d4fcaae64c0f9aa35b495cc4f734
vumac <hoangvutruong27894@gmail.com>
2026-07-14 16:27:54 +0800

    Cleanup

--- projects/test-ssr/src/app/app.config.ts
+++ projects/test-ssr/src/app/app.config.ts
@@ -1,17 +1,9 @@
-import {
-  provideHttpClient,
-  withFetch,
-  withInterceptors,
-} from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
-import {
-  provideClientHydration,
-  withEventReplay,
-  withNoHttpTransferCache,
-} from '@angular/platform-browser';
+import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -21,7 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(
-      withFetch(),
       withInterceptors([
         (req, next) => {
           const reqWithCredentials = req.clone({ credentials: 'include' });
@@ -29,6 +20,6 @@ export const appConfig: ApplicationConfig = {
         },
       ]),
     ),
-    provideClientHydration(withEventReplay(), withNoHttpTransferCache()),
+    provideClientHydration(),
   ],
 };
